fc398250f40887b9e1a25f87545207f6fec76146
RossaMania <raclettenberg@gmail.com>
2024-05-07 15:24:35 -0500

    feat: Add if statements and console writeline messages based on expiration date under each comment block.

--- SubscriptionRenewalApp/Program.cs
+++ SubscriptionRenewalApp/Program.cs
@@ -9,15 +9,40 @@ int discountPercentage = 0;
 
 //  If the user's subscription will expire in 10 days or less, display the message: Your subscription will expire soon. Renew now!
 
+if (daysUntilExpiration <= 10)
+{
+    Console.WriteLine("Your subscription will expire soon. Renew now!");
+}
+
 //  If the user's subscription will expire in five days or less, display the messages: Your subscription expires in _ days.
 // Renew now and save 10%!
 // Be sure to replace the _ character displayed in the message above with the value stored in the variable daysUntilExpiration when you construct your message output.
+
+if (daysUntilExpiration <= 5)
+{
+    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days. Renew now and save 10%!");
+}
 
 // If the user's subscription will expire in one day, display the messages:
 // Your subscription expires within a day!
 // Renew now and save 20%!
 
+if (daysUntilExpiration == 1)
+{
+    Console.WriteLine("Your subscription expires within a day! Renew now and save 20%!");
+}
+
 // If the user's subscription has expired, display the message:
 // Your subscription has expired.
 
+if (daysUntilExpiration == 0)
+{
+    Console.WriteLine("Your subscription has expired.");
+}
+
 // If the user's subscription doesn't expire in 10 days or less, display nothing.
+
+if (daysUntilExpiration > 10)
+{
+    Console.WriteLine("");
+}
